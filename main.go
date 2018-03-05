f39905c0d98caccaebc47fd66215882354567e07
t-kusakabe <ttmac0704@gmail.com>
2018-03-05 15:39:00 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "Set github access token."},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+	client := github.NewClient(tc)
+
+	repos, _, _ := client.Repositories.List(ctx, "", nil)
+
+	fmt.Println(repos)
+}
